fb2582b8400ad83faa69ce2db6fc461234fa41a9
Charlotte <weiht@shanghaitech.edu.cn>
2020-12-04 21:56:28 +0800

    Completed crawler

--- data_source/fr24_crawler.py
+++ data_source/fr24_crawler.py
@@ -1,40 +1,41 @@
+import json
+import requests
+import time
 from typing import List, Tuple, Dict, Any
-import requests, json
 
 
 class Fr24Crawler:
 
-    def __init__(self, loc: Tuple[float, float], rng: float):
+    def __init__(self, loc: Tuple[float, float], rng: Tuple[float, float]):
         # loc[0]是搜索中心的纬度latitude
         # loc[1]是经度longitude
-        # rng在README上的定义非常ambiguous，姑且认为是搜索正方形边长的1/2，单位是nm海里
-        # 1 n mile = 1852 m
-        self.__center_latitude = loc[0]
-        self.__center_longitude = loc[1]
-        self.__min_latitude = None
-        self.__min_longitude = None
-        self.__max_latitude = None
-        self.__max_longitude = None
+        # rng在README上的定义非常ambiguous 按照西北角坐标处理
+        d_latitude = loc[0] - rng[0]
+        d_longitude = rng[1] - loc[1]
+        min_latitude = loc[0] - d_latitude
+        min_longitude = loc[1] - d_longitude
+        max_latitude = loc[0] + d_latitude
+        max_longitude = loc[1] + d_longitude
         # bounds和headers用于后面的url
         self.__bounds = 'bounds={},{},{},{}&faa=1&satellite=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&' \
-                        'estimated=1&maxage=14400&gliders=1&stats=1'.format(self.__max_latitude, self.__min_latitude,
-                                                                            self.__min_longitude, self.__max_longitude)
+                        'estimated=1&maxage=14400&gliders=1&stats=1'.format(max_latitude, min_latitude,
+                                                                            min_longitude, max_longitude)
         self.__headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/87.0.4280.88 Safari/537.36',
             'Host': 'data-live.flightradar24.com'}
         # 后面步骤需要存储为一个文件 这里根据parameter先确定文件名（其实也可以挪到后面去？）
-        self.__filename = "{},{},{}.json".format(loc[0], loc[1], rng)
+        self.__filename = "{},{},{},{}.json".format(loc[0], loc[1], rng[0], rng[1])
 
     def get_data_once(self):
-        response = requests.get('https://data-live.flightradar24.com/zones/fcgi/feed.js' + self.__bounds,
+        r = requests.get('https://data-live.flightradar24.com/zones/fcgi/feed.js' + self.__bounds,
                                 headers=self.__headers)
-        # output是return或保存到文件的内容的容器
-        output = []
-        # 删除前两行没用的数据 以便之后for循环
+        response = json.loads(r.text)
         del response['full_count']
         del response['version']
-
+        pass
+        # output是return或保存到文件的内容的容器
+        output = []
         for flight in response:
             # 每一个flight里面的内容依次是：
             # 0  ICAO 24-bit address      str      国际民航组织24位飞机地址（不需要）
@@ -56,10 +57,14 @@ class Fr24Crawler:
             # 16 airline flight number    str      非IATA航班号（不需要）
             # 17 unknown 0 or 1           int/bool 未知（不需要？）
             # 18 airline                  str      航空公司（不需要）
-            output.append({'longitude': flight[1], 'latitude': flight[2], 'heading': flight[3], 'altitude': flight[4],
-                           'ground speed': flight[5], 'squawk number': flight[6], 'registration number': flight[9],
-                           'flight number': flight[13], 'departure airport': flight[11], 'arrival airport': flight[12]})
+            output.append({'longitude': response[flight][1], 'latitude': response[flight][2], 'heading': response[flight][3], 'altitude': response[flight][4],
+                           'ground speed': response[flight][5], 'squawk number': response[flight][6], 'registration number': response[flight][9],
+                           'response number': response[flight][13], 'departure airport': response[flight][11], 'arrival airport': response[flight][12]})
         f = open(self.__filename, mode='w')
-        f.write(json.dump(output))
+        f.write(json.dumps(output))
+        # 表示Request成功
+        print('响应状态码：', r.status_code)
     def spin(self, interval=10):
-        raise NotImplementedError
+        while True:
+            self.get_data_once()
+            time.sleep(interval)
